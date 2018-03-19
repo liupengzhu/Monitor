5419615e7d6e5dffb5f816e837e608b392f3f952
朱留鹏 <>
2018-03-19 13:14:35 +0800

    区间用电量界面布局实现及点击事件实现

--- app/src/main/java/cn/com/larunda/monitor/fragment/GasFragment.java
+++ app/src/main/java/cn/com/larunda/monitor/fragment/GasFragment.java
@@ -1,10 +1,200 @@
 package cn.com.larunda.monitor.fragment;
 
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.highlight.Highlight;
+
+import cn.com.larunda.dialog.DateDialog;
+import cn.com.larunda.monitor.R;
+import cn.com.larunda.monitor.util.BarChartViewPager;
+import cn.com.larunda.monitor.util.BarOnClickListener;
+import cn.com.larunda.monitor.util.MyApplication;
+import cn.com.larunda.monitor.util.Util;
 
 /**
  * Created by sddt on 18-3-19.
  */
 
-public class GasFragment extends Fragment {
+public class GasFragment extends Fragment implements View.OnClickListener {
+    private final String gas_URL = MyApplication.URL + "gas/usage" + MyApplication.TOKEN;
+    private String date_type = "month";
+    private String type = "original";
+
+    private SharedPreferences preferences;
+    public static String token;
+    private BarChartViewPager mBarChart;
+    private TextView textView1;
+
+    private DateDialog yearDialog;
+    private DateDialog monthDialog;
+    private TextView dateText;
+    private RadioButton yearButton;
+    private RadioButton monthButton;
+    private RadioGroup timeGroup;
+
+    private RadioGroup typeGroup;
+    private RadioButton originalButton;
+    private RadioButton foldButton;
+
+    private SwipeRefreshLayout refreshLayout;
+    private LinearLayout layout;
+    private LinearLayout errorLayout;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_gas, container, false);
+        initView(view);
+        initData();
+        initEvent();
+        return view;
+    }
+
+    /**
+     * 初始化view
+     *
+     * @param view
+     */
+    private void initView(View view) {
+        preferences = PreferenceManager.getDefaultSharedPreferences(getContext());
+        token = preferences.getString("token", null);
+        textView1 = view.findViewById(R.id.gas_fragment_chart_text);
+        yearDialog = new DateDialog(getContext(), false, false);
+        monthDialog = new DateDialog(getContext(), true, false);
+        dateText = view.findViewById(R.id.gas_date_text);
+        yearButton = view.findViewById(R.id.gas_fragment_year_button);
+        monthButton = view.findViewById(R.id.gas_fragment_month_button);
+        timeGroup = view.findViewById(R.id.gas_fragment_time_group);
+
+        typeGroup = view.findViewById(R.id.gas_fragment_type_group);
+        originalButton = view.findViewById(R.id.gas_fragment_original_button);
+        foldButton = view.findViewById(R.id.gas_fragment_fold_button);
+
+        layout = view.findViewById(R.id.gas_fragment_layout);
+        errorLayout = view.findViewById(R.id.gas_fragment_error_layout);
+
+        refreshLayout = view.findViewById(R.id.gas_fragment_swipe);
+        refreshLayout.setColorSchemeColors(getResources().getColor(R.color.colorPrimary));
+        refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                sendRequest();
+            }
+        });
+    }
+
+    /**
+     * 初始化数据
+     */
+    private void initData() {
+        long time = System.currentTimeMillis();
+        String date = Util.parseTime(time, 2);
+        dateText.setText(date);
+    }
+
+    /**
+     * 初始化点击事件
+     */
+    private void initEvent() {
+        yearButton.setOnClickListener(this);
+        monthButton.setOnClickListener(this);
+
+        originalButton.setOnClickListener(this);
+        foldButton.setOnClickListener(this);
+
+        dateText.setOnClickListener(this);
+
+        yearDialog.setOnCancelClickListener(new DateDialog.OnCancelClickListener() {
+            @Override
+            public void OnClick(View view) {
+                yearDialog.cancel();
+            }
+        });
+        monthDialog.setOnCancelClickListener(new DateDialog.OnCancelClickListener() {
+            @Override
+            public void OnClick(View view) {
+                monthDialog.cancel();
+            }
+        });
+
+
+        yearDialog.setOnOkClickListener(new DateDialog.OnOkClickListener() {
+            @Override
+            public void OnClick(View view, String date) {
+                if (dateText != null && date != null) {
+                    dateText.setText(date);
+                    sendRequest();
+                }
+                yearDialog.cancel();
+            }
+        });
+        monthDialog.setOnOkClickListener(new DateDialog.OnOkClickListener() {
+            @Override
+            public void OnClick(View view, String date) {
+                if (dateText != null && date != null) {
+                    dateText.setText(date);
+                    sendRequest();
+                }
+                monthDialog.cancel();
+            }
+        });
+
+
+    }
+
+    /**
+     * 点击事件处理
+     *
+     * @param v
+     */
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.gas_fragment_year_button:
+                long time1 = System.currentTimeMillis();
+                String date1 = Util.parseTime(time1, 1);
+                dateText.setText(date1);
+                sendRequest();
+                break;
+            case R.id.gas_fragment_month_button:
+                long time2 = System.currentTimeMillis();
+                String date2 = Util.parseTime(time2, 2);
+                dateText.setText(date2);
+                sendRequest();
+                break;
+
+            case R.id.gas_fragment_original_button:
+                sendRequest();
+                break;
+            case R.id.gas_fragment_fold_button:
+                sendRequest();
+                break;
+            case R.id.gas_date_text:
+                if (timeGroup.getCheckedRadioButtonId() == R.id.gas_fragment_year_button) {
+                    yearDialog.show();
+                } else if (timeGroup.getCheckedRadioButtonId() == R.id.gas_fragment_month_button) {
+                    monthDialog.show();
+                }
+                break;
+        }
+    }
+
+    /**
+     * 发送请求
+     */
+    private void sendRequest() {
+    }
 }

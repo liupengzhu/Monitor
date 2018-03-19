c67af8410eef1c1c2c617ec888156f5c1c9aa3a6
朱留鹏 <>
2018-03-19 13:35:28 +0800

    区间用气量界面柱状图数据获取

--- app/src/main/java/cn/com/larunda/monitor/fragment/GasFragment.java
+++ app/src/main/java/cn/com/larunda/monitor/fragment/GasFragment.java
@@ -18,6 +18,7 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.components.AxisBase;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.highlight.Highlight;
 
@@ -35,11 +36,15 @@ import cn.com.larunda.monitor.bean.WaterBean;
 import cn.com.larunda.monitor.gson.GasInfo;
 import cn.com.larunda.monitor.gson.WaterInfo;
 import cn.com.larunda.monitor.util.ActivityCollector;
+import cn.com.larunda.monitor.util.BarChartManager;
 import cn.com.larunda.monitor.util.BarChartViewPager;
 import cn.com.larunda.monitor.util.BarOnClickListener;
 import cn.com.larunda.monitor.util.HttpUtil;
 import cn.com.larunda.monitor.util.MyApplication;
 import cn.com.larunda.monitor.util.Util;
+import cn.com.larunda.monitor.util.XValueFormatter;
+import cn.com.larunda.monitor.util.XYMarkerView;
+import cn.com.larunda.monitor.util.YValueFormatter;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.Response;
@@ -77,6 +82,8 @@ public class GasFragment extends Fragment implements View.OnClickListener {
     private GasRecyclerAdapter adapter;
     private LinearLayoutManager manager;
     private List<GasBean> gasBeanList = new ArrayList<>();
+    private BarChartManager barManager;
+    private XYMarkerView barMarkerView;
 
     @Nullable
     @Override
@@ -133,6 +140,35 @@ public class GasFragment extends Fragment implements View.OnClickListener {
         manager = new LinearLayoutManager(getContext());
         recyclerView.setLayoutManager(manager);
         recyclerView.setAdapter(adapter);
+
+        mBarChart = view.findViewById(R.id.gas_fragment_chart);
+        barManager = new BarChartManager(mBarChart);
+        barManager.setDescription("");
+        barManager.setxValueFormatter(new XValueFormatter() {
+            @Override
+            public String getFormattedValue(float value, AxisBase axis) {
+                if (date_type.equals("year")) {
+                    return (int) value + "月";
+                } else {
+                    return (int) value + "日";
+                }
+            }
+        });
+        barManager.setyValueFormatter(new YValueFormatter() {
+            @Override
+            public String getFormattedValue(float value, AxisBase axis) {
+                if (type.equals("original")) {
+                    return (int) Math.ceil(value) + " " + preferences.getString("gas_unit", null) + "";
+                } else {
+                    return (int) Math.ceil(value) + " tce";
+                }
+
+            }
+        });
+
+        barMarkerView = new XYMarkerView(getContext());
+        barMarkerView.setChartView(mBarChart);
+        mBarChart.setMarker(barMarkerView);
     }
 
     /**
@@ -188,6 +224,33 @@ public class GasFragment extends Fragment implements View.OnClickListener {
                     sendRequest();
                 }
                 monthDialog.cancel();
+            }
+        });
+
+        barMarkerView.setBarOnClickListener(new BarOnClickListener() {
+            @Override
+            public void onClick(Entry e, Highlight highlight, View v) {
+                if (v instanceof TextView) {
+                    StringBuffer content = new StringBuffer();
+                    if (e.getY() > 0) {
+                        v.setVisibility(View.VISIBLE);
+                    } else {
+                        v.setVisibility(View.GONE);
+                    }
+                    if (e.getX() < 10) {
+                        content.append("时间:" + dateText.getText().toString() + "-0" + (int) e.getX() + "\r\n");
+                    } else {
+                        content.append("时间:" + dateText.getText().toString() + "-" + (int) e.getX() + "\r\n");
+                    }
+                    if (type.equals("original")) {
+
+                        content.append("用气量:" + e.getY() + preferences.getString("gas_unit", null) + "");
+                    } else {
+                        content.append("用气量:" + e.getY() + "tce");
+                    }
+
+                    ((TextView) v).setText(content.toString());
+                }
             }
         });
 
@@ -288,6 +351,25 @@ public class GasFragment extends Fragment implements View.OnClickListener {
      * @param gasInfo
      */
     private void parseGas(GasInfo gasInfo) {
+        if (gasInfo.getChart() != null) {
+            float values[] = new float[gasInfo.getChart().size()];
+            for (int i = 0; i < gasInfo.getChart().size(); i++) {
+                String value = gasInfo.getChart().get(i);
+                if (value != null) {
+                    values[i] = Float.valueOf(value);
+                }
+            }
+            barManager.showBarChart(values, "", getResources().getColor(R.color.water_color));
+        }
+
+        if (date_type.equals("year")) {
+            textView1.setText(dateText.getText().toString() + "年 区间用气量柱状图");
+        } else {
+            textView1.setText(dateText.getText().toString().split("-")[0] + "年"
+                    + dateText.getText().toString().split("-")[1]
+                    + "月 区间用气量柱状图");
+        }
+
         gasBeanList.clear();
         if (gasInfo.getTable_data() != null) {
             for (GasInfo.TableDataBean bean : gasInfo.getTable_data()) {

e45cbd43cdf11e67ae9824960651e1d440583082
朱留鹏 <>
2018-03-28 10:45:20 +0800

    用水界面添加菜单

--- app/src/main/java/cn/com/larunda/monitor/WaterActivity.java
+++ app/src/main/java/cn/com/larunda/monitor/WaterActivity.java
@@ -1,12 +1,19 @@
 package cn.com.larunda.monitor;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 
 import android.os.Bundle;
+import android.support.v7.view.menu.MenuBuilder;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,6 +33,7 @@ public class WaterActivity extends BaseActivity implements View.OnClickListener 
     private WaterFragmentAdapter adapter;
 
     private Button backButton;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +42,9 @@ public class WaterActivity extends BaseActivity implements View.OnClickListener 
         init();
         initView();
         initEvent();
+        setSupportActionBar(toolbar);
     }
+
     /**
      * 初始化信息
      */
@@ -65,6 +75,9 @@ public class WaterActivity extends BaseActivity implements View.OnClickListener 
                 Util.setIndicator(tabLayout, 50, 50);
             }
         });
+
+        toolbar = findViewById(R.id.water_toolbar);
+        toolbar.setTitle("");
     }
 
     /**
@@ -85,4 +98,72 @@ public class WaterActivity extends BaseActivity implements View.OnClickListener 
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.water_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.water_menu_electric:
+                Intent electricIntent = new Intent(this, ElectricActivity.class);
+                startActivity(electricIntent);
+                finish();
+                break;
+            case R.id.water_menu_water:
+                Intent waterIntent = new Intent(this, WaterActivity.class);
+                startActivity(waterIntent);
+                finish();
+                break;
+            case R.id.water_menu_steam:
+                Intent steamIntent = new Intent(this, SteamActivity.class);
+                startActivity(steamIntent);
+                finish();
+                break;
+            case R.id.water_menu_gas:
+                Intent gasIntent = new Intent(this, GasActivity.class);
+                startActivity(gasIntent);
+                finish();
+                break;
+            case R.id.water_menu_renewable:
+                Intent renewableIntent = new Intent(this, RenewableActivity.class);
+                startActivity(renewableIntent);
+                finish();
+                break;
+            case R.id.water_menu_carbon:
+                Intent carbonIntent = new Intent(this, CarbonActivity.class);
+                startActivity(carbonIntent);
+                finish();
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * 反射修改弹出菜单显示图标
+     *
+     * @param view
+     * @param menu
+     * @return
+     */
+    @SuppressLint("RestrictedApi")
+    @Override
+    protected boolean onPrepareOptionsPanel(View view, Menu menu) {
+        if (menu != null) {
+            if (menu.getClass() == MenuBuilder.class) {
+                try {
+                    Method m = menu.getClass().getDeclaredMethod("setOptionalIconsVisible", Boolean.TYPE);
+                    m.setAccessible(true);
+                    m.invoke(menu, true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return super.onPrepareOptionsPanel(view, menu);
+    }
 }
